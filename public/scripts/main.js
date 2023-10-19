f0ca81375d5b244f715da3dedbcbd7f6d65c37ca
wyattlake <lakewyatt@gmail.com>
2023-10-18 19:54:53 -0700

    Added base terminal functionality

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,28 +1,149 @@
-class Main {
-    constructor() {
-        this.pageViewsKey = 'pageViewsCount';
-        this.initializeCounter();
-        this.displayCount();
-    }
+const previousCommands = [];
+commandIdx = 0;
 
-    initializeCounter() {
-        if (!localStorage.getItem(this.pageViewsKey)) {
-            localStorage.setItem(this.pageViewsKey, '0');
-        }
-    }
-
-    incrementCount() {
-        let currentCount = parseInt(localStorage.getItem(this.pageViewsKey));
-        currentCount++;
-        localStorage.setItem(this.pageViewsKey, currentCount.toString());
-    }
-
-    displayCount() {
-        this.incrementCount();
-        // Update count in div id count
-        document.getElementById('count').innerHTML = 'You have visited this page ' + localStorage.getItem(this.pageViewsKey)  + ' times.'
+class FileObject {
+    constructor(name, content, children) {
+        this.name = name;
+        this.content = content;
+        this.children = children;
     }
 }
 
-// Note that we construct the class here, but we don't need to assign it to a variable.
-document.mainClass = new Main();
+function createFile(name, content) {
+    return new FileObject(name, content, null);
+}
+
+function createFolder(name, children) {
+    return new FileObject(name, "", children);
+}
+
+function getFile(filename, directory) {
+    if (directory == null) {
+        for (i = 0; i < files.length; i++) {
+            if (files[i].name == filename) {
+                return files[i];
+            }
+        }
+    } else {
+        for (i = 0; i < directory.children.length; i++) {
+            if (directory.children[i].name == filename) {
+                return directory.children[i];
+            }
+        }
+    }
+
+    return null;
+}
+
+const files = [
+    createFile("file.txt", "files"),
+    createFile("file2.txt", "files2"),
+    createFolder("folder", [
+        createFile("file3.txt", "files3"),
+        createFile("file4.txt", "files4"),
+    ]),
+];
+
+function listFiles(directory) {
+    result = "";
+    if (directory == null) {
+        for (i = 0; i < files.length; i++) {
+            result += files[i].name + "\n";
+        }
+    } else {
+        for (i = 0; i < directory.children.length; i++) {
+            result += directory.children[i].name + "\n";
+        }
+    }
+    return result;
+}
+
+function parsePath(input) {
+    inputFiles = input.split("/");
+    parentFolder = null;
+
+    fileIdx = 0;
+    while (fileIdx < inputFiles.length) {
+        currentFile = inputFiles[fileIdx];
+        file = getFile(currentFile, parentFolder);
+
+        if (file != null) {
+            if (fileIdx + 1 == inputFiles.length) {
+                return file;
+            } else {
+                parentFolder = file;
+                fileIdx++;
+                continue;
+            }
+        } else {
+            return null;
+        }
+    }
+}
+
+function parseInput(input) {
+    const words = input.split(" ");
+    if (words.length > 0) {
+        switch (words[0]) {
+            case "ls":
+                if (words.length == 1) {
+                    return listFiles(null);
+                } else if (words.length == 2) {
+                    directory = getFile(words[1]);
+                    if (directory != null) {
+                        return listFiles(directory);
+                    } else {
+                        return "Invalid path";
+                    }
+                }
+                break;
+            case "cat":
+                if (words.length == 2) {
+                    file = parsePath(words[1]);
+                    if (file != null) {
+                        if (file.children == null) {
+                            return file.content;
+                        }
+                        return file.name + " is a directory";
+                    } else {
+                        return "Invalid path";
+                    }
+                }
+                break;
+            case "clear":
+                return null;
+            case "help":
+                return "ls - lists files in a directory\ncat - reads files\nclear - clears the console";
+        }
+    }
+    return "Invalid command";
+}
+
+document.addEventListener("keypress", function (event) {
+    const inputElement = document.getElementById("myText");
+    const textDiv = document.getElementById("previousText");
+
+    if (event.key === "Enter") {
+        const oldCommand = document.createElement("p");
+        oldCommand.textContent = "wyattlake > " + inputElement.value;
+        textDiv.appendChild(oldCommand);
+
+        previousCommands.push(oldCommand.value);
+        commandIdx++;
+
+        const parseResult = parseInput(inputElement.value);
+
+        if (parseResult == null) {
+            textDiv.innerHTML = "";
+        } else {
+            const parseSplits = parseResult.split("\n");
+            for (i = 0; i < parseSplits.length; i++) {
+                const response = document.createElement("p");
+                response.textContent = parseSplits[i];
+                textDiv.appendChild(response);
+            }
+        }
+
+        inputElement.value = "";
+    }
+});
